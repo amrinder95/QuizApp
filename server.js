9092759cd5fda67e87ac475195cb61c41be44824
animward <animward@gmail.com>
2024-04-07 14:57:38 -0700

    added nodemon, connect ejs to servers

--- server.js
+++ server.js
@@ -52,6 +52,15 @@ app.get('/users', (req, res) => {
   res.render('users');
 });
 
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
