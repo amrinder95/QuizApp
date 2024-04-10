f2553c1333b92a67f7805f52c3864331c8bc97ea
animward <animward@gmail.com>
2024-04-10 16:08:51 -0700

    dont allow registration if username already exists

--- routes/register.js
+++ routes/register.js
@@ -11,8 +11,13 @@ router.post("/", async (req, res) => {
   try {
     const { username, password } = req.body; 
     const newUser = await usersQueries.createUser(username, password);
+    
+    if (newUser.error) {
+      res.render("register", { error: newUser.error });
+    } else {
     console.log("New user:", newUser); // for testing REMOVE LATER
     res.redirect("/login");
+    }
   } catch (error) {
     console.error("Error registering user:", error);
     res.status(500).send("Internal Server Error!");
