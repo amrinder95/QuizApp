ea57100ea22ff02444eb173ed06e9ddf9ee0dfb8
Amrinder Singh <amrinder995@gmail.com>
2024-04-10 17:54:10 -0400

    fix header not showing logged in on certain pages

--- routes/quiz.js
+++ routes/quiz.js
@@ -7,12 +7,12 @@ const { getQuestionsByQuizId } = require('../db/queries/questions')
 
 router.get('/quiz/:id', async (req, res) => {
     const id = req.params.id;
-
+    const username = req.session.username;
     try {
         const quiz = await getQuizById(id);
         const questions = await getQuestionsByQuizId(id);
-        
-        res.render('quiz', { quiz, questions });
+
+        res.render('quiz', { quiz, questions, username });
     } catch (error) {
         console.error('Error fetching quiz by ID:', error);
         res.status(500).send('Internal Server Error');
