e779672caaead7c320597127cefc5f383d56009a
Amrinder Singh <amrinder995@gmail.com>
2024-04-11 21:21:55 -0400

    fix add quesiton button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,12 @@
 // public/scripts/app.js
 // Client facing scripts here
-import { questionsForQuiz } from '../db/queries/questions';
+//import { questionsForQuiz } from '../db/queries/questions';
 
 // add a new question button
 document.addEventListener("DOMContentLoaded", () => {
   const addQuestionBtn = document.getElementById("add-question-btn");
   const questionsContainer = document.getElementById("questions-container");
-  const submitBtn = document.querySelector("button[type='submit']");
+  const submitBtn = document.querySelector("button[id='submit-btn']");
 
   if (addQuestionBtn && questionsContainer) {
     addQuestionBtn.addEventListener("click", () => {
@@ -18,21 +18,14 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
-document.getElementById('quiz-form').addEventListener('submit', async function(event) {
-  event.preventDefault();
+// document.getElementById('quiz-form').addEventListener('submit', async function(event) {
+//   event.preventDefault();
 
-try {
-  const questions = await questionsForQuiz();
-  console.log(questions);
-} catch (error) {
-  console.error('Error fetching questions:', error);
-}
-});
-
- /*const questionsAndAnswers = await questions.questionsForQuiz();
- document.getElementById('quiz-form').addEventListener('submit'), function(event) {
-   event.preventDefault();
-
-   const selectedAnswers
- }*/
+// try {
+//   const questions = await questionsForQuiz();
+//   console.log(questions);
+// } catch (error) {
+//   console.error('Error fetching questions:', error);
+// }
+// });
 
